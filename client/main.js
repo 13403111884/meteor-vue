8c68ec0972dc3f4590e7236e79c4aca781795b94
13403111884 <13403111884@163.com>
2019-09-13 01:36:05 +0800

    UI框架替换

--- client/main.js
+++ client/main.js
@@ -1,19 +1,18 @@
-// import { Meteor } from 'meteor/meteor'
 import Vue from 'vue'
 import VueMeteorTracker from 'vue-meteor-tracker'
 import router from './../imports/client/router'
 import store from './../imports/client/store'
-import ConfigUI from './configUI'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
 
 import App from './../imports/client/App.vue'
 
-import "@quasar/extras/material-icons/material-icons.css"
-import "quasar/dist/quasar.min.css"
 
 Vue.use(VueMeteorTracker)
-Vue.use(ConfigUI)
 
 Meteor.startup(() => {
+  Vue.use(Vant)
   new Vue({
     router,
     store,
